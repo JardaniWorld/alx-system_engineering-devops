ac02d6d21dcc22861c97519b66a1f71016049969
JardaniWorld <Jonchukwuma04@gmail.com>
2022-10-14 14:50:13 -0700

    This code creates a file in a directory using Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file {'/tmp/school': name => '/tmp/school'
-ensure => /tmp/school
-permission => 0744
-owner => www-data
-group => www-data
+# This Puppet code creates a file
+
+file {'/tmp/school':
+ensure => 'present',
+permission => '0744',
+owner => 'www-data',
+group => 'www-data',
 content => 'I love Puppet'
 }
